0a4fc5dd70f692ec3297bd71873474fa74f1c4a8
payce06 <paycegrossman@gmail.com>
2025-01-19 19:02:12 -0500

    for-loops-go

--- for-loops.go
+++ for-loops.go
@@ -27,3 +27,45 @@ func main() {
             break
         }
     }
+
+    // ----- Using continue -----
+    fmt.Println("\nUsing continue:")
+    for i := 0; i < 5; i++ {
+        if i%2 == 0 {
+            continue // Skip even numbers
+        }
+        fmt.Println("Odd number:", i)
+    }
+
+    // ----- Using range with slices -----
+    fmt.Println("\nUsing range with a slice:")
+    nums := []int{10, 20, 30, 40, 50}
+    for index, value := range nums {
+        fmt.Printf("Index: %d, Value: %d\n", index, value)
+    }
+
+    // ----- Using range with maps -----
+    fmt.Println("\nUsing range with a map:")
+    colors := map[string]string{
+        "red":   "#FF0000",
+        "green": "#00FF00",
+        "blue":  "#0000FF",
+    }
+    for key, value := range colors {
+        fmt.Printf("Key: %s, Value: %s\n", key, value)
+    }
+
+    // ----- Using range with a string -----
+    fmt.Println("\nUsing range with a string:")
+    for index, char := range "GoLang" {
+        fmt.Printf("Index: %d, Character: %c\n", index, char)
+    }
+
+    // ----- Nested loops -----
+    fmt.Println("\nNested loops:")
+    for i := 1; i <= 3; i++ {
+        for j := 1; j <= 3; j++ {
+            fmt.Printf("i=%d, j=%d\n", i, j)
+        }
+    }
+}
