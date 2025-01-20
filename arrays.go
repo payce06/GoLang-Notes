450ebdbc4a6fcec15c086c675374d51669964399
payce06 <paycegrossman@gmail.com>
2025-01-19 19:58:31 -0500

    arrays-go

--- arrays.go
+++ arrays.go
@@ -26,3 +26,34 @@ func main() {
     for i := 0; i < len(arr2); i++ {
         fmt.Printf("arr2[%d] = %d\n", i, arr2[i])
     }
+
+    fmt.Println("\nIterating with range:")
+    for index, value := range arr3 {
+        fmt.Printf("Index: %d, Value: %s\n", index, value)
+    }
+
+    // ----- 4. Using the len function -----
+    fmt.Printf("\nLength of Array 1: %d\n", len(arr1))
+    fmt.Printf("Length of Array 3: %d\n", len(arr3))
+
+    // ----- 5. Copying arrays -----
+    fmt.Println("\nCopying arrays:")
+    copyArr := arr2 // Copies the entire array
+    copyArr[0] = 999
+    fmt.Println("Original Array 2:", arr2)
+    fmt.Println("Copied Array (modified):", copyArr)
+
+    // ----- 6. Multidimensional arrays -----
+    fmt.Println("\nMultidimensional arrays:")
+    var matrix [2][3]int // A 2x3 array
+    matrix[0][0] = 1
+    matrix[1][2] = 5
+    fmt.Println("Matrix:", matrix)
+
+    fmt.Println("Iterating over Matrix:")
+    for i := 0; i < len(matrix); i++ {
+        for j := 0; j < len(matrix[i]); j++ {
+            fmt.Printf("matrix[%d][%d] = %d\n", i, j, matrix[i][j])
+        }
+    }
+}
