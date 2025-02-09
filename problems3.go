8526fc41982f2464619afa98dc979b10575f7ab4
payce06 <paycegrossman@gmail.com>
2025-02-08 21:07:10 -0500

    problems3-go

--- problems3.go
+++ problems3.go
@@ -9,10 +9,10 @@ import (
 // 1. Function to find the maximum number in an array
 func findMaximum(arr []int) int {
 maximum := arr[0]
-for _, num := range arr{
-    if num > maximum{
-        maximum = num
-    }
+for _, num := range arr {
+if num > maximum {
+maximum = num
+}
 }
 return maximum
 }
@@ -21,7 +21,7 @@ return maximum
 func removeDuplicates(arr []int) []int {
 unique := []int{}
 seen := map[int]bool{}
-for _, item := range array{
+for _, item := range arr {
 if !seen[item] {
 seen[item] = true
 unique = append(unique, item)
@@ -30,24 +30,23 @@ unique = append(unique, item)
 return unique
 }
 
-
 // 3. Function that takes an array of numbers and returns a new array with each number squared
 func squareElements(arr []int) []int {
-	sqaured := []int{}
-	for _, num := range array {
-	squared = append(squared, num*num)
-	}
-	return squared
-	}
-	
-	// 4. Function to sort an array of strings in alphabetical order
-	func sortStrings(arr []string) []string {
-	for i := 0; i < len(arr); i++ {
-	for j := j + 1; j < len(arr); j++ {
-	if arr[i] > arr[j] {
-	arr[i], arr[j] = arr[j], arr[i]
-	}
-	}
-	}
-	return arr
-	}
+squared := []int{}
+for _, num := range arr {
+squared = append(squared, num*num)
+}
+return squared
+}
+
+// 4. Function to sort an array of strings in alphabetical order
+func sortStrings(arr []string) []string {
+for i := 0; i < len(arr); i++ {
+for j := i + 1; j < len(arr); j++ {
+if arr[i] > arr[j] {
+arr[i], arr[j] = arr[j], arr[i]
+}
+}
+}
+return arr
+}
