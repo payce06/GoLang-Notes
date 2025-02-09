f10b4a8f214d1a1242162ccecb8bc573afafd096
payce06 <paycegrossman@gmail.com>
2025-02-08 21:07:34 -0500

    problems3-go

--- problems3.go
+++ problems3.go
@@ -50,3 +50,32 @@ arr[i], arr[j] = arr[j], arr[i]
 }
 return arr
 }
+
+
+// 5. Function that returns the last element of an array
+func lastElement(arr []int) int {
+	if len(arr) == 0 {
+	return -1 // Return -1 for empty arrays
+	}
+	return arr[len(arr)-1]
+	}
+	
+	// 6. Function to find the median of an array of numbers
+	func findMedian(arr []int) float64 {
+	n := len(arr)
+	if n == 0 {
+	return 0
+	}
+	// Sort the array first
+	for i := 0; i < n; i++ {
+	for j := i + 1; j < n; j++ {
+	if arr[i] > arr[j] {
+	arr[i], arr[j] = arr[j], arr[i]
+	}
+	}
+	}
+	if n%2 == 0 {
+	return float64(arr[n/2-1]+arr[n/2]) / 2.0
+	}
+	return float64(arr[n/2])
+	}
