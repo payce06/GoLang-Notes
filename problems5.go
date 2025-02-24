768d8f8f7d8f57c5d76a1ea8bba283e76914f0c0
payce06 <paycegrossman@gmail.com>
2025-02-23 20:59:08 -0500

    problems5-go

--- problems5.go
+++ problems5.go
@@ -16,3 +16,25 @@ func mergeObjects(obj1, obj2 map[string]interface{}) map[string]interface{} {
 	}
 	return merged
 }
+
+// 2. Count the number of properties in an object
+func countProperties(obj map[string]interface{}) int {
+	return len(obj)
+}
+
+// 3. Get an array of values for a specific property in an array of objects
+func getPropertyValues(arr []map[string]interface{}, prop string) []interface{} {
+	var values []interface{}
+	for _, obj := range arr {
+		if value, ok := obj[prop]; ok {
+			values = append(values, value)
+		}
+	}
+	return values
+}
+
+// 4. Check if an object contains a specific property
+func containsProperty(obj map[string]interface{}, prop string) bool {
+	_, exists := obj[prop]
+	return exists
+}
