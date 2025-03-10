13e74ca5327a160203dd346e4a61db685884c30a
payce06 <paycegrossman@gmail.com>
2025-03-10 14:25:34 -0400

    student-go

--- student.go
+++ student.go
@@ -11,3 +11,16 @@ type Course struct {
 	CourseCode string
 	Students   []*Student // Slice to store enrolled students
 }
+
+
+// EnrollStudent method to enroll a student in a course
+func (c *Course) EnrollStudent(student *Student) {
+	for _, s := range c.Students {
+		if s.StudentID == student.StudentID {
+			fmt.Printf("%s is already enrolled in %s.\n", student.Name, c.CourseName)
+			return
+		}
+	}
+	c.Students = append(c.Students, student)
+	fmt.Printf("%s has been enrolled in %s.\n", student.Name, c.CourseName)
+}
